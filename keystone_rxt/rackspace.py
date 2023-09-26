aed3b8b7fc77a811412701ab203a76cdf903826d
Kevin Carter <kevin.carter@rackspace.com>
2023-09-26 12:40:26 -0500

    Initial Commit
    Signed-off-by: Kevin Carter <kevin.carter@rackspace.com>

---
+++ keystone_rxt/rackspace.py
@@ -0,0 +1,103 @@
+# Copyright 2013 OpenStack Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import requests
+
+import flask
+
+from keystone.auth.plugins import base
+from keystone.auth.plugins import mapped
+from keystone.common import provider_api
+import keystone.conf
+from keystone import exception
+from keystone.i18n import _
+
+
+PROVIDERS = provider_api.ProviderAPIs
+RACKPSACE_IDENTITY_V2 = "https://identity.api.rackspacecloud.com/v2.0/tokens"
+
+
+# The keystone config options used by this module and forced to be RXT
+keystone.conf.CONF.set_override("assertion_prefix", "RXT", group="federation")
+
+
+class RXT(base.AuthMethodHandler):
+    def authenticate(self, auth_payload):
+        """Turn a signed request with an access key into a keystone token."""
+        # Check the headers for v1 auth
+        auth_payload["identity_provider"] = "rackspace"
+        auth_payload["protocol"] = "rackspace"
+        return self._v2(auth_payload=auth_payload)
+
+    def _v1(self, headers):
+        raise exception.AuthMethodNotSupported(method="RackspaceV1")
+
+    def _v2(self, auth_payload):
+        try:
+            auth_data = {
+                "auth": {
+                    "RAX-KSKEY:apiKeyCredentials": {
+                        "username": auth_payload["user"]["name"],
+                        "apiKey": auth_payload["user"]["apikey"],
+                    }
+                }
+            }
+            r = requests.post(RACKPSACE_IDENTITY_V2, json=auth_data)
+            r.raise_for_status()
+            return_data = r.json()
+        except (
+            requests.HTTPError,
+            requests.ConnectionError,
+            requests.exceptions.JSONDecodeError,
+            KeyError,
+        ):
+            raise exception.Unauthorized(
+                _("Could not validate the access token")
+            )
+
+        access = return_data["access"]
+        access_user = access["user"]
+        access_token = access["token"]
+        flask.request.environ["RXT_UserName"] = access_user["name"]
+        flask.request.environ["RXT_Email"] = access_user["email"]
+        flask.request.environ["RXT_DomainID"] = access_user[
+            "RAX-AUTH:domainId"
+        ]
+        flask.request.environ["RXT_TenantName"] = access_token["tenant"][
+            "name"
+        ]
+        flask.request.environ["RXT_TenantID"] = access_token["tenant"]["id"]
+
+        orgPersonType = set(
+            "Rackspace:Cloud:User",
+        )
+        for role in return_data["access"]["user"]["roles"]:
+            orgPersonType.add(role["name"])
+        else:
+            flask.request.environ["RXT_orgPersonType"] = ";".join(
+                orgPersonType
+            )
+
+        response_data = mapped.handle_unscoped_token(
+            auth_payload,
+            PROVIDERS.resource_api,
+            PROVIDERS.federation_api,
+            PROVIDERS.identity_api,
+            PROVIDERS.assignment_api,
+            PROVIDERS.role_api,
+        )
+
+        return base.AuthHandlerResponse(
+            status=True, response_body=None, response_data=response_data
+        )
