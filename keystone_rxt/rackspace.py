4270563f55e1458514db8f3954059d9ea43b9205
Kevin Carter <kevin.carter@rackspace.com>
2023-10-02 12:14:08 -0500

    Fix license header
    Signed-off-by: Kevin Carter <kevin.carter@rackspace.com>

--- keystone_rxt/rackspace.py
+++ keystone_rxt/rackspace.py
@@ -1,4 +1,4 @@
-# Copyright 2023 Cloudnull
+# Copyright 2023 Cloudnull <kevin@cloudnull.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
